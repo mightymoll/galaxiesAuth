23ee5458628c3ee34c119518d1e63afcbb971f9f
molly <mnfelts@gmail.com>
2025-03-15 10:56:14 +0100

    login page logic
    - state & fn setups to be used in login process

--- app/screens/Login.tsx
+++ app/screens/Login.tsx
@@ -1,7 +1,31 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text } from 'react-native';
+import React, {useState} from 'react';
+import {useAuth} from '../context/AuthContext'
 
 const Login = () => {
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
+	const {onLogin, onRegister} = useAuth();
+
+// alert if login is not successful
+	const login = async ()=>{
+		const result = await onLogin!(email,password);
+		if(result && result.error){
+			alert(result.msg);
+		}
+	}
+
+// automatically call login after a successful registration
+	const register = async()=>{
+		const result = await onRegister!(email, password);
+    if(result && result.error){
+      alert(result.msg);
+    }
+    else{
+      login();
+    }
+	}
+
 	return (
 		<View>
 			<Text>Login</Text>
