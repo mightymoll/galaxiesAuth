eca58cf9fd4eee18fa10484a24564d0c05178316
molly <mnfelts@gmail.com>
2025-03-15 10:48:09 +0100

    apply auth to app.tsx
    - wrap app in AuthProvider to secure unathorized users from accessing it
    - create navigation container with home and login pages

--- App.tsx
+++ App.tsx
@@ -1,20 +1,45 @@
+import { NavigationContainer } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import {AuthProvider, useAuth} from './app/context/AuthContext';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import Home from './app/screens/Home'
+import Login from './app/screens/Login'
+
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <AuthProvider>
+      <Layout>
+        
+      </Layout>
+    </AuthProvider>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export const Layout = ()=>{
+  const {authState, onLogout} = useAuth();
+
+  return (
+    <NavigationContainer>
+      {/* define screens availible in app */}
+      <Stack.Navigator>
+      {/* check that user is authenticated before going to other pages */}
+{authState?.authenticated ? (
+  <Stack.Screen 
+  name="Home" 
+  component={Home}
+  options={
+    {headerRight:()=><Button onPress={onLogout} title="Sign Out" />,
+    }}></Stack.Screen>
+    ):(
+  <Stack.Screen name="Login" component={Login}></Stack.Screen>
+  )}
+</Stack.Navigator>
+    </NavigationContainer>
+  )
+}
+
